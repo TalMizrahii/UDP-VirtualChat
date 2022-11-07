9af650c287b2561d0bd305e1ca312cfb4cba0408
TalMizrahii <Taltalon1927@gmail.com>
2022-11-07 18:45:00 +0200

    base server and client upload

--- client.py
+++ client.py
@@ -1,1 +1,10 @@
-print("hello23")
+import socket
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.sendto(b'Tal Mizrahi 206960890, Yuval Arbel 206945107', ('10.0.2.15', 12345))
+
+data, addr = s.recvfrom(1024)
+print(str(data), addr)
+
+s.close()
