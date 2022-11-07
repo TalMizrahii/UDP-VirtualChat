aede1c6d59234dfd1301df82ef6b57ad2173ff5c
TalMizrahii <Taltalon1927@gmail.com>
2022-11-07 20:51:47 +0200

    server initialization

--- client.py
+++ client.py
@@ -4,11 +4,12 @@ import sys
 server_ip = str(sys.argv[1])
 server_port = int(sys.argv[2])
 
-msg = input()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True:
+    msg = input()
+
     s.sendto(msg.encode(), (server_ip, server_port))
     data, addr = s.recvfrom(1024)
 
